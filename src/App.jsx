83879b63ee9d7575acf68c1e50d93beaf8ad3a1e
Mandeep Singh Kachawa <mandeepsingh9773@gmail.com>
2023-10-27 13:47:53 +0530

    "Created the SignInPage"

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import { Button } from '@mui/material';
+import SignInPage from "./Components/SignInPage/SignInPage";
 
 function App() {
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <h1>Crafted with 💜 by Lucif3r-in</h1>
-      <Button color='secondary'>Secondary</Button>
-      <Button variant='contained' color='success'>
-        Success
-      </Button>
-      <Button variant='outlined' color='error'>
-        Error
-      </Button>
+    <div>
+  <SignInPage />
     </div>
   );
 }
