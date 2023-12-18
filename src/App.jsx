7016b15b4a46112eeac2dfc9401185cf71c6a1b3
Lucif3r-in <ashutosh123rath@gmail.com>
2023-12-18 17:08:51 +0530

    fix: separate customer side

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './Components/SignInPage/SignInPage';
 import LayoutPage from './Components/Layout/LayoutPage';
-import Signup from './pages/signup';
-import OtpVerification from './pages/otpverification';
-import FinishSignup from './pages/finishsignup';
-import HomePage from './pages/homepage';
-import ThreePartMultipage from './pages/multipage';
-import UploadPhoto from './pages/uploadphoto';
-import DisplayPhoto from './pages/displayphoto';
+
 // admin section
-import AdminSignInPage from './AdminPanel/SignInPage/SignInPage'
-import AdminPanelSidenav from './AdminPanel/Sidenav/AdminPanelSidenav'
+import AdminSignInPage from './AdminPanel/SignInPage/SignInPage';
+import AdminPanelSidenav from './AdminPanel/Sidenav/AdminPanelSidenav';
 import { useEffect } from 'react';
 
 function App() {
@@ -22,19 +16,11 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/signin' element={<SignInPage />} />
+          <Route path='/' element={<SignInPage />} />
           <Route path='/merchant' element={<LayoutPage />} />
           <Route path='/customer' element={<LayoutPage />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/otp' element={<OtpVerification />} />
-          <Route path='/finish' element={<FinishSignup />} />
-          <Route path='/' element={<HomePage />} />
-          <Route path='/multipage' element={<ThreePartMultipage />} />
-          <Route path='/uploadphoto' element={<UploadPhoto />} />
-          <Route path='/displayphoto' element={<DisplayPhoto />} />
           <Route path='/admin/signin' element={<AdminSignInPage />} />
           <Route path='/admin/dashboard' element={<AdminPanelSidenav />} />
-          
         </Routes>
       </BrowserRouter>
     </>
