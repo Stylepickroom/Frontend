a7c420dad76632d79e80fab962b2ada64bdb30f3
Somvit09 <developer.09.sam@gmail.com>
2024-01-10 16:49:25 +0530

    added frontend

--- src/host/host.js
+++ src/host/host.js
@@ -1,3 +1,4 @@
-const HostServer = "https://node-backend-production-7612.up.railway.app";
+// const HostServer = "https://node-backend-production-7612.up.railway.app";
+const HostServer = "http://127.0.0.1:5000";
 
 export default HostServer;
