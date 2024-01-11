7b57e23419d362aa9017d95c30a9436dbcad7eaf
Somvit09 <developer.09.sam@gmail.com>
2024-01-11 11:19:50 +0530

    changed host

--- src/host/host.js
+++ src/host/host.js
@@ -1,4 +1,4 @@
-// const HostServer = "https://node-backend-production-7612.up.railway.app";
-const HostServer = "http://127.0.0.1:5000";
+const HostServer = "https://node-backend-production-7612.up.railway.app";
+// const HostServer = "http://127.0.0.1:5000";
 
 export default HostServer;
